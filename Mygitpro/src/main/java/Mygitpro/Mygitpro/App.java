8c8b75921ad95bf4b19798da54caa1e554b4f210
Madhusmitha07 <Madhusmithareddy1993@gmail.com>
2022-12-21 21:37:34 +0530

    My first commit

---
+++ Mygitpro/src/main/java/Mygitpro/Mygitpro/App.java
@@ -0,0 +1,13 @@
+package Mygitpro.Mygitpro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
